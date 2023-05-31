d36db4a3689ec1da5a781a5a9c919a55b4157ac1
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2023-05-31 17:26:41 -0600

    Agregar archivos de proyecto.

---
+++ Sesion_2/Persona.cs
@@ -0,0 +1,26 @@
+﻿namespace Sesion_2
+{
+    public class Persona
+    {
+        public Persona()
+        {
+           Nombre=string.Empty;
+            Edad = 0;
+        }
+        public string Nombre { get; set; }
+        public int Edad { get; set;}
+
+        private void setNombre(string Nombre)
+        {
+            this.Nombre = Nombre;
+        }
+
+        public int GetEdad()
+        {
+            setNombre("Antonio");
+            return Edad;
+        }
+
+       
+    }
+}
---
+++ Sesion_2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Sesion_2;
+
+Persona persona = new Persona();
+
+persona.Nombre = "Marvin";
+persona.Edad = 50;
+
+Console.WriteLine(persona.GetEdad());
+persona.Nombre = "Marvin";
+Console.WriteLine(persona.Nombre);
+Console.ReadLine();
